0960bee2ada22fd84b1ac039ad6da301403047c9
kazuki1023 <k.keio12569@gmail.com>
2022-12-25 17:37:55 +0900

    モーダルの中身完成

--- web/script/piechart.js
+++ web/script/piechart.js
@@ -1,4 +1,78 @@
 "use strict";
+// 棒グラフの作成
+{
+  var options = {
+    series: [{
+    name: 'Servings',
+    data: [44, 55, 41, 67, 22, 43, 21, 33, 45, 31, 87, 65, 35]
+  }],
+    annotations: {
+    points: [{
+      x: 'Bananas',
+      seriesIndex: 0,
+      label: {
+        borderColor: '#775DD0',
+        offsetY: 0,
+        style: {
+          color: '#fff',
+          background: '#775DD0',
+        },
+        text: 'Bananas are good',
+      }
+    }]
+  },
+  chart: {
+    height: 350,
+    type: 'bar',
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 10,
+      columnWidth: '50%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: 2
+  },
+  grid: {
+    row: {
+      colors: ['#fff', '#f2f2f2']
+    }
+  },
+  xaxis: {
+    labels: {
+      rotate: -45
+    },
+    categories: ['Apples', 'Oranges', 'Strawberries', 'Pineapples', 'Mangoes', 'Bananas',
+      'Blackberries', 'Pears', 'Watermelons', 'Cherries', 'Pomegranates', 'Tangerines', 'Papayas'
+    ],
+    tickPlacement: 'on'
+  },
+  yaxis: {
+    title: {
+      text: 'Servings',
+    },
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'light',
+      type: "horizontal",
+      shadeIntensity: 0.25,
+      gradientToColors: undefined,
+      inverseColors: true,
+      opacityFrom: 0.85,
+      opacityTo: 0.85,
+      stops: [50, 0, 100]
+    },
+  }
+  };
+  var chart = new ApexCharts(document.getElementById("hours_chart"), options);
+  chart.render();
+}
 
 // 学習言語の円グラフの作成
 {
@@ -33,11 +107,11 @@
       responsive: false,
     }
   }],
-  
+
   };
 
-  var chart = new ApexCharts(document.getElementById('learning_character_chart'), options);
-  chart.render();
+  var characterchart = new ApexCharts(document.getElementById('learning_character_chart'), options);
+  characterchart.render();
 }
 
 // 学習コンテンツの円グラフの作成
@@ -67,8 +141,8 @@
   }]
   };
 
-  var chart = new ApexCharts(document.getElementById('learning_content_chart'), options);
-  chart.render();
+  var contentchart = new ApexCharts(document.getElementById('learning_content_chart'), options);
+  contentchart.render();
 }
 
 
