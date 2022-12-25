de60cf4ac43238d8b5792745becf7ba362a2f951
kazuki1023 <k.keio12569@gmail.com>
2022-12-25 10:16:16 +0900

    間違ったメールアドレス入れるとログインできなくする

--- src/admin/login_judge.php
+++ src/admin/login_judge.php
@@ -12,8 +12,9 @@ $loginsql = "SELECT * FROM users WHERE email = :email";
 $stmt = $dbh->prepare($loginsql);
 $stmt->bindValue(':email', $email);
 $stmt->execute();
-$member = $stmt->fetch();
-// 
+$member = $stmt->fetchAll();
+
+// 最初に設定したパスワードはハッシュ化シてないので、そのパスワードのチェック
 if (isset($_POST['password'], $member['password'])){
     //DBのユーザー情報をセッションに保存
     $_SESSION['id'] = $member['id'];
@@ -24,17 +25,22 @@ if (isset($_POST['password'], $member['password'])){
     $msg = 'メールアドレスもしくはパスワードが間違っています。';
     $link = '<a href="login.php">戻る</a>';
 };
-//指定したハッシュがパスワードにマッチしているかチェック
-if (password_verify($_POST['password'], $member['password'])){
-    //DBのユーザー情報をセッションに保存
-    $_SESSION['id'] = $member['id'];
-    $_SESSION['name'] = $member['name'];
-    $msg = 'ログインしました。';
-    $link = '<a href="http://localhost:8080/admin/index.php">ホーム</a>';
+// 指定したハッシュがパスワードにマッチしているかチェック
+if (isset($_POST['email'],$member[0]['email'])){
+    if (password_verify($_POST['password'], $member[0]['password'])){
+        //DBのユーザー情報をセッションに保存
+        $_SESSION['id'] = $member[0]['id'];
+        $_SESSION['name'] = $member[0]['name'];
+        $msg = 'ログインしました。';
+        $link = '<a href="http://localhost:8080/admin/index.php">ホーム</a>';
+    } else {
+        $msg = 'メールアドレスもしくはパスワードが間違っています。';
+        $link = '<a href="login.php">戻る</a>';
+    };
 } else {
-    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $msg = 'メールアドレスが間違ってます';
     $link = '<a href="login.php">戻る</a>';
-};
+}
 
 echo $msg;
 echo "<br>";
