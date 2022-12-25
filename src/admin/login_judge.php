4bf7c0e81da7e465829c7e5e88cc2dd9b56a05a5
kazuki1023 <k.keio12569@gmail.com>
2022-12-25 10:39:27 +0900

    ログイン完成

--- src/admin/login_judge.php
+++ src/admin/login_judge.php
@@ -31,15 +31,17 @@ if (isset($_POST['email'],$member[0]['email'])){
         //DBのユーザー情報をセッションに保存
         $_SESSION['id'] = $member[0]['id'];
         $_SESSION['name'] = $member[0]['name'];
-        $msg = 'ログインしました。';
-        $link = '<a href="http://localhost:8080/admin/index.php">ホーム</a>';
+        // ログインできたときのメッセージ作成
+        $msg = 'ログインできました';
+        $link ='<a href="http://localhost:8080/admin/index.php">管理者画面へ</a>';
     } else {
         $msg = 'メールアドレスもしくはパスワードが間違っています。';
-        $link = '<a href="login.php">戻る</a>';
+        $link = '<a href="login.php">ログイン画面に戻る</a>';
     };
 } else {
+    $name = $_SESSION['name'];
     $msg = 'メールアドレスが間違ってます';
-    $link = '<a href="login.php">戻る</a>';
+    $link = '<a href="login.php">ログイン画面に戻る</a>';
 }
 
 echo $msg;
--- src/admin/register.php
+++ src/admin/register.php
@@ -26,3 +26,21 @@ try {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>新規登録画面</title>
+</head>
+<body>
+  <main>
+  <h1>新規登録完了しました</h1>
+  <div>
+    <a href="login.php">ログイン画面に戻る</a>
+  </div>
+  </main>
+</body>
+</html>
